1c84965857c80764fbf9e77c37650d475be9a7a3
Danil Ikusov <danil.ikusov@gmail.com>
2021-11-17 23:15:46 +0700

    Some minor changes

--- src/main/java/ru/ikusov/training/telegrambot/MyConfig.java
+++ src/main/java/ru/ikusov/training/telegrambot/MyConfig.java
@@ -1,10 +1,8 @@
 package ru.ikusov.training.telegrambot;
 
-import com.google.j2objc.annotations.Property;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("ru.ikusov.training.telegrambot")
