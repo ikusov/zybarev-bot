b1d51f8f9603ffb4bf460d5f2477f3f4508564b1
Danil Ikusov <danil.ikusov@gmail.com>
2021-12-04 23:15:38 +0700

    Fixed crash by null (JDBC_DATABASE_URL was not set; changed to parsing DATABASE_URL variant)

--- src/main/java/ru/ikusov/training/telegrambot/services/DatabaseConnector.java
+++ src/main/java/ru/ikusov/training/telegrambot/services/DatabaseConnector.java
@@ -6,16 +6,27 @@ import org.springframework.stereotype.Component;
 import ru.ikusov.training.telegrambot.model.Quote;
 
 import javax.annotation.PreDestroy;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Component
 public class DatabaseConnector {
     private final SessionFactory sessionFactory;
 
-    public DatabaseConnector() {
-        String connectionUrl = System.getenv("JDBC_DATABASE_URL");
+    public DatabaseConnector() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String connectionUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+//        String connectionUrl = System.getenv("JDBC_DATABASE_URL");
 
         Configuration configuration = new Configuration();
         configuration.setProperty("hibernate.connection.url", connectionUrl);
+        configuration.setProperty("hibernate.connection.username", username);
+        configuration.setProperty("hibernate.connection.password", password);
+
         configuration.configure("hibernate.cfg.xml");
         configuration.addAnnotatedClass(Quote.class);
 
