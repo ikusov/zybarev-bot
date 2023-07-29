f26ebc6d06a2416f3c99c546c13dcd361b8523be
Danil Ikusov <danil.ikusov@gmail.com>
2023-07-29 23:49:47 +0700

    ZYB-003 перевёл одну сущность в lombok

--- src/main/java/ru/ikusov/training/telegrambot/model/wordle/WordleChatAttemptListEntity.java
+++ src/main/java/ru/ikusov/training/telegrambot/model/wordle/WordleChatAttemptListEntity.java
@@ -4,40 +4,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.ikusov.training.telegrambot.model.CommonEntity;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "wordle_chat_attempt_list")
 public class WordleChatAttemptListEntity implements CommonEntity {
-    public Long getChatId() {
-        return chatId;
-    }
-
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
-
-    public List<String> getAttemptList() {
-        return attemptList;
-    }
-
-    public void setAttemptList(List<String> attemptList) {
-        this.attemptList = attemptList;
-    }
-
-    public WordleChatAttemptListEntity(Long chatId, List<String> attemptList) {
-        this.chatId = chatId;
-        this.attemptList = attemptList;
-    }
-
-    public WordleChatAttemptListEntity() {
-    }
-
     @Id
     @Column(name = "chat_id")
     private Long chatId;
+
     @Column(name = "attempt_list")
     private List<String> attemptList;
 }
